41abc473f4678f30b3e9748bd3aedec1c87e01b1
Quentin Goinaud <armaldio@gmail.com>
2018-03-08 15:42:47 +0100

    fix: catch promise reject error message

--- scripts/butler-upload.js
+++ scripts/butler-upload.js
@@ -5,11 +5,13 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 async function windowsUpload () {
-    let command            = `${process.cwd()}/ext-bin/butler.exe push "build/${pkg.build.productName} Setup ${pkg.version}.exe" armaldio/construct-addon-installer:windows --userversion ${pkg.version} `;
+    let command = `${process.cwd()}/ext-bin/butler.exe push "build/${pkg.build.productName} Setup ${pkg.version}.exe" armaldio/construct-addon-installer:windows --userversion ${pkg.version} `;
     console.log(command);
     const {stdout, stderr} = await exec(command);
     console.log('stdout:', stdout);
     console.log('stderr:', stderr);
 }
 
-windowsUpload();
+windowsUpload().then().catch(err => {
+    console.log(err);
+});
