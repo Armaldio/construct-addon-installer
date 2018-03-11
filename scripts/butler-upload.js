3d278617e24c2a5aa46405d0266d6967b2b65bf1
Quentin Goinaud <armaldio@gmail.com>
2018-03-11 13:31:37 +0100

    deploy on master push release build
    deploy on develop push nightly build

--- scripts/butler-upload.js
+++ scripts/butler-upload.js
@@ -4,8 +4,8 @@ const fs  = require('fs');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-async function windowsUpload () {
-    let command = `${process.cwd()}/ext-bin/butler.exe push "build/${pkg.build.productName} Setup ${pkg.version}.exe" armaldio/construct-addon-installer:windows --userversion ${pkg.version} `;
+async function windowsUpload (branch, build) {
+    let command = `${process.cwd()}/ext-bin/butler.exe push "build/${pkg.build.productName} Setup ${pkg.version}${branch !== 'master' ? '.' + build : ''}.exe" armaldio/construct-addon-installer:${branch === 'master' ? 'windows' : 'windows:nightly'} --userversion ${pkg.version}${branch !== 'master' ? '.' + build : ''}`;
     console.log(command);
     const {stdout, stderr} = await exec(command);
     console.log('stdout:', stdout);
@@ -16,10 +16,12 @@ let branch       = process.env.APPVEYOR_REPO_BRANCH;
 let buildNumber  = process.env.APPVEYOR_BUILD_NUMBER;
 let buildVersion = process.env.APPVEYOR_BUILD_VERSION;
 
-console.log("branch: " + branch);
-console.log("buildNumber: " + buildNumber);
-console.log("buildVersion: " + buildVersion);
+console.log('branch: ' + branch);
+console.log('buildNumber: ' + buildNumber);
+console.log('buildVersion: ' + buildVersion);
 
-windowsUpload().then().catch(err => {
-    console.log(err);
-});
+if (branch === 'develop' || branch === 'master') {
+    windowsUpload(branch, buildNumber).then().catch(err => {
+        console.log(err);
+    });
+}
